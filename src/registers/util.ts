909978c63e0d4cac21c7235a53894158485368a4
Carson Storm <stormymcstorm@gmail.com>
2019-03-13 14:40:16 -0600

    add utils

---
+++ src/registers/util.ts
@@ -0,0 +1,34 @@
+/**
+ * Creates a bit mask form a array of booleans
+ * 
+ * @param arr the state of each bit. {@code true} for 1 and {@code false} for 0
+ * @returns {number}  the bit mask
+ */
+export function maskFromArray(arr: boolean[]): number {
+  if (arr.length > 32)
+    throw new RangeError('cannot create a bitmask longer than 32 bits');
+
+  let bm = 0;
+
+  for (let i = 0; i < arr.length; bm |= +arr[i] << i++);
+
+  return bm;
+}
+
+/**
+ * Creates a boolean array from a bit mask
+ * 
+ * @param mask the mask to create a boolean array from
+ * @returns {boolean[]} the boolean array
+ */
+export function arrayFromMask(mask: number): boolean[] {
+  if (mask < (1 << 31) || mask > ~(1 << 31))
+    throw new RangeError('mask is out of range: ' + mask);
+
+  const arr = [];
+
+  for (let shifted = mask; shifted; shifted >>>= 1)
+    arr.push((shifted & 1) == 1);
+
+  return arr;
+}
