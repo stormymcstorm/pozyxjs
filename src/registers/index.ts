93563412360cd1aba6ac1e7eba97fe67eb1700f3
Carson Storm <stormymcstorm@gmail.com>
2019-03-13 15:21:10 -0600

    add index for registers

---
+++ src/registers/index.ts
@@ -0,0 +1,18 @@
+import Register, {ReadRegister, ReadWriteRegister} from './Register';
+import * as Status from './StatusRegisters';
+import * as Config from './ConfigRegisters';
+import * as Sensor from './SensorRegisters';
+import * as General from './GeneralRegisters';
+
+export {
+  Register,
+  ReadRegister,
+  ReadWriteRegister,
+};
+
+export {
+  Status,
+  Config,
+  Sensor,
+  General,
+};
