728035abcacb71c4fa9a87675d991fe666c85930
Carson Storm <stormymcstorm@gmail.com>
2019-03-13 19:23:53 -0600

    add accessors for device status

--- src/Pozyx.ts
+++ src/Pozyx.ts
@@ -1,11 +1,19 @@
 import Connection from "./connections/Connection";
-import {Status, Config, Function} from "./Registers";
+import { ReadRegister, ReadWriteRegister, Status } from "./registers";
+
+const EXPECTED_WHO_AM_I = 0x43
 
 /**
  * A pozyx device
  */
 export default class Pozyx {
   private _connection: Connection;
+
+  private _firmwareVersion: [number, number];
+  private _hardwareVersion: number;
+  private _isShield: boolean;
+  private _selfTestResults: Status.SelfTestResult;
+
   private _isInitialized = false;
 
   constructor(connection: Connection) {
@@ -20,47 +28,67 @@ export default class Pozyx {
   }
 
   /**
+   * Returns whether or not this device is an arduino shield
+   * 
+   * @returns {@code true} if this device is an arduino shield, {@code false} 
+   *                       otherwise
+   */
+  public isShield(): boolean {
+    this._assertInitialized();
+
+    return this._isShield;
+  }
+
+  /**
+   * Returns a tuple representing the firmware version
+   * 
+   * @returns a tuple representing the firmware version. The first value is the
+   *          major version and the second value is the minor version
+   */
+  public getFirmwareVersion(): [number, number] {
+    this._assertInitialized();
+
+    return this._firmwareVersion;
+  }
+
+  /**
+   * Returns the hardware version of the device
+   * 
+   * @returns the hardware version
+   */
+  public getHardwareVersion(): number {
+    this._assertInitialized();
+
+    return this._hardwareVersion;
+  }
+
+  /**
+   * Returns the results of the device's self test
+   * 
+   * @returns the results of the device's self test
+   */
+  public getSelfTestResults(): Status.SelfTestResult {
+    this._assertInitialized();
+
+    return this._selfTestResults;
+  }
+
+  /**
    * Initializes the connection with the device
    */
   public init(): Promise<void> {
     return this._connection.init()
       .then(() => this.ping())
+      .then(() => this._readRegisters([Status.FIRMWARE_VERSION, 
+        Status.HARDWARE_VERSION, Status.SELF_TEST_RESULTS]))
+      .then(data => { // get device info
+        this._firmwareVersion = data[0];
+
+        [this._hardwareVersion, this._isShield] = data[1];
+
+        this._selfTestResults = data[2];
+      })
       .then(() => {this._isInitialized = true});
-  }
-  
-  /**
-   * Sets the states of the LEDs. Behavior is not defined if LEDs are not owned
-   * @see {@link Pozyx#setLEDOwnership}
-   * 
-   * @param states  the states to set. True for on, false for off.
-   * @returns {Promise} resolved when the LED states are set
-   */
-  public setLEDStates(states: boolean[]): Promise<any> {
-    const stateBitmap = booleanArrayToBitmap(states) & 0b1111; // only consider bit 0..4
-
-    return this._connection.call(Function.LED_CONTROL, Buffer.from([0b11110000 + stateBitmap]), 1);
-  }
-
-  /**
-   * Sets the ownership of LEDs 1 through 4. See POZYX_CONFIG_LEDS register description 
-   * <a href="https://www.pozyx.io/product-info/developer-tag/datasheet-register-overview">here</a>
-   * for information on what LEDs mean when owned by pozyx
-   * 
-   * @param claims  A list specifying the ownership of the led. True for user ownership, 
-   *                false for pozyx ownership.
-   * @returns {Promise} resolved when the led's ownership has been changed
-   */
-  public async setLEDOwnership(claims: boolean[]): Promise<any> {
-    const claimBitmap = (booleanArrayToBitmap(claims) & 0b1111) ^ 0b1111; // only consider bit 0..4 and toggle bits
-    const data = await this._connection.read(Config.LED_CONFIGURATION, 1);
-    const confBitmap = data.readUInt8(0);
-
-    if ((confBitmap & 0b1111) == claimBitmap) // write is not necessary
-      return;
-
-    data.writeUInt8((confBitmap & ~0b1111) + claimBitmap, 0);
-
-    return this._connection.write(Config.LED_CONFIGURATION, data);
   }
 
   /**
@@ -70,27 +98,56 @@ export default class Pozyx {
    *                    connection cannot be verified.
    */
   public async ping(): Promise<void> {
-    if (await this.getWhoAmI() != 0x43)
+    if (await this.getWhoAmI() != EXPECTED_WHO_AM_I)
       throw new Error('Failed to ping pozyx');
+  }
+
+  /**
+   * Reads from the register
+   * 
+   * @param register the register to read
+   * @returns {Promise} resolved to the data stored at the register
+   */
+  public async readReg<T>(register: ReadRegister<T>): Promise<T> {
+    const buf = await this._connection.read(register.address, register.size);
+    return register.decode(buf);
+  }
+
+  /**
+   * Writes the data to the register
+   * 
+   * @param register the register to write to
+   * @param data the data to write
+   * @returns {Promise} resolved when the write is complete
+   */
+  public async writeReg<T>(register: ReadWriteRegister<T>, data: T): Promise<any> {
+    const buf = register.encode(data);
+    return this._connection.write(register.address, buf);
   }
 
   /**
    * Gets the value stored at the WHO_AM_I register. Used to verify connection
    * to device
+   * 
    * @returns {Promise<number>}
    */
-  public async getWhoAmI(): Promise<number> {
-    const data = await this._connection.read(Status.WHO_AM_I, 1);
-
-    return data.readUInt8(0);
+  public getWhoAmI(): Promise<number> {
+    return this.readReg(Status.WHO_AM_I);
   }
-}
 
-function booleanArrayToBitmap(arr: boolean[]) {
-  let bm = 0;
+  private _assertInitialized() {
+    if (! this._isInitialized)
+      throw new Error('Device has not been initialized');
+  }
 
-  for (let i = 0; i < arr.length; i++)
-    if (arr[i]) bm += 1 << i;
+  private async _readRegisters(registers: ReadRegister<any>[]): Promise<any[]> {
+    const last = registers[registers.length - 1];
+    const buf = await this._connection.read(registers[0].address, last.address + last.size);
+    const data = [];
 
-  return bm;
+    for (let i = 0, offset = 0; i < registers.length; offset += registers[i++].size)
+      data[i] = registers[i].decode(buf.slice(offset, offset + registers[i].size));
+
+    return data;
+  }
 }
