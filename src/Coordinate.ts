9e219ee5eb5bf2bda79d7cbf5e18b7e7a877e5bd
Carson Storm <stormymcstorm@gmail.com>
2019-03-15 18:33:37 -0600

    add coordinate structure

---
+++ src/Coordinate.ts
@@ -0,0 +1,19 @@
+/**
+ * A 3 dimensional coordinate
+ */
+export default interface Coordinate {
+  /**
+   * The x coordinate measured in mm. Must be a int32
+   */
+  x: number,
+
+  /**
+   * The y coordinate measured in mm. Must be a int32
+   */
+  y: number,
+
+  /**
+   * The z coordinate measured in mm. Must be a int32
+   */
+  z: number,
+}
